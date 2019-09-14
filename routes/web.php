82e170f5dd88c1db2ca325ab038596ecba7bc6b8
Bias Tegaralaga <tegaralaga@live.com>
2019-09-15 02:47:53 +0700

    Initial Commit for Routing

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => '/', 'namespace' => 'Rest'], function () use ($router) {
+    $router->get('/', ['as' => 'index', 'uses' => 'MiscellaneousController@index']);
+    $router->group(['prefix' => 'api'], function () use ($router) {
+        $router->get('/', ['uses' => 'MiscellaneousController@index']);
+        $router->group(['prefix' => 'v1'], function () use ($router) {
+            $router->get('/', ['uses' => 'MiscellaneousController@index']);
+            $router->group(['prefix' => 'location'], function () use ($router) {
+                $router->get('/', ['uses' => 'MiscellaneousController@index']);
+                $router->get('/{id:[0-9]+}', ['uses' => 'VenueController@index']);
+                $router->post('create', ['uses' => 'VenueController@create']);
+            });
+        });
+    });
 });
