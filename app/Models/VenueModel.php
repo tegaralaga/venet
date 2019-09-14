fb2c31cb3357848c490e441ff6047b8d0d45a229
Bias Tegaralaga <tegaralaga@live.com>
2019-09-14 22:38:57 +0700

    Add spatial support @ VenueModel

--- app/Models/VenueModel.php
+++ app/Models/VenueModel.php
@@ -7,11 +7,18 @@
  */
 
 namespace App\Models;
+use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
+
+/**
+ * @property \Grimzy\LaravelMysqlSpatial\Types\Point   $ven_coordinate
+ */
 
 use Illuminate\Database\Eloquent\Model;
 
 class VenueModel extends Model
 {
+    use SpatialTrait;
+
     protected $fillable = [
         'ven_parent_id',
         'ven_vty_id',
@@ -21,6 +28,10 @@ class VenueModel extends Model
         'ven_address',
         'ven_name',
         'ven_description',
+        'ven_coordinate',
+    ];
+    protected $spatialFields = [
+        'ven_coordinate',
     ];
     protected $primaryKey = 'ven_id';
     protected $table = 'tbl_venue';
