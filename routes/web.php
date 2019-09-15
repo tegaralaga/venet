5d73b9793b6cd450203c1a71eb83d280f8240eeb
Bias Tegaralaga <tegaralaga@live.com>
2019-09-16 05:52:06 +0700

    Modify Routing

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,27 @@ $router->group(['prefix' => '/', 'namespace' => 'Rest'], function () use ($route
             $router->get('/', ['uses' => 'MiscellaneousController@index']);
             $router->group(['prefix' => 'location'], function () use ($router) {
                 $router->get('/', ['uses' => 'MiscellaneousController@index']);
-                $router->get('/{id:[0-9]+}', ['uses' => 'VenueController@index']);
+                $router->get('/{id:[0-9]+}', ['uses' => 'VenueController@venue_info']);
                 $router->post('create', ['uses' => 'VenueController@create']);
                 $router->post('contact/create', ['uses' => 'VenueController@create_contact']);
                 $router->post('venue_type/create', ['uses' => 'VenueController@create_venue_type']);
             });
             $router->group(['prefix' => 'event'], function () use ($router) {
                 $router->get('/', ['uses' => 'MiscellaneousController@index']);
-                $router->get('/{id:[0-9]+}', ['uses' => 'EventController@index']);
+                $router->post('ticket/create', ['uses' => 'EventController@ticket_create']);
+                $router->get('ticket/{id:[0-9A-Za-z]+}', ['uses' => 'EventController@ticket_info']);
                 $router->post('create', ['uses' => 'EventController@create']);
-                $router->post('ticket/create', ['uses' => 'EventController@create_ticket']);
+                $router->post('toggle', ['uses' => 'EventController@toggle']);
                 $router->post('contact/create', ['uses' => 'EventController@create_contact']);
                 $router->post('type/create', ['uses' => 'EventController@create_type']);
                 $router->post('tag/create', ['uses' => 'EventController@create_tag']);
                 $router->post('rule/create', ['uses' => 'EventController@create_rule']);
+                $router->get('{id:[0-9A-Za-z]+}', ['uses' => 'EventController@event_info']);
             });
             $router->group(['prefix' => 'transaction'], function () use ($router) {
                 $router->get('/', ['uses' => 'MiscellaneousController@index']);
-                $router->get('/{id:[0-9]+}', ['uses' => 'TransactionController@index']);
                 $router->post('purchase', ['uses' => 'TransactionController@purchase']);
+                $router->get('{id:[0-9A-Za-z]+}', ['uses' => 'TransactionController@transaction_info']);
             });
             $router->group(['prefix' => 'line_up'], function () use ($router) {
                 $router->get('/', ['uses' => 'MiscellaneousController@index']);
